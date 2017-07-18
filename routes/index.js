111a0b87ad9df8a8d19fef53b34a5587ae947719
Krishna <krishna@Saurabhs-MacBook-Pro.local>
2017-07-18 13:49:34 +0530

    Create all necessary routes and all jade files

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/habitat', function(req, res, next) {
+  res.render('habitat')
+})
+
+router.get('/cookies', function(req, res, next) {
+  res.render('cookies')
+})
+
+router.get('/logging', function(req, res, next) {
+  res.render('logging')
+})
+
+router.get('/web-sockets', function(req, res, next) {
+  res.render('web-sockets')
+})
+
+
 module.exports = router;
